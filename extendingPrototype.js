fe842f989e75b8a99bd95bbef9b96ac5877b2bff
Chizi-P <61510435+Chizi-P@users.noreply.github.com>
2020-09-19 02:41:52 +0800

    Update extendingPrototype.js

--- extendingPrototype.js
+++ extendingPrototype.js
@@ -131,44 +131,89 @@ Array.create2d = function create2d(len1, len2) {
     return new Array(len1).fillCopy(len2 == undefined ? [] : new Array(len2));
 }
 Array.create3d = function create3d(len1, len2, len3) {
-    return new Array(len1).fillCopy(new Array(len2).fillCopy(len2 == undefined ? [] : new Array(len3)));
+    return new Array(len1).fillCopy(new Array(len2).fillCopy(len3 == undefined ? [] : new Array(len3)));
 }
 Array.create4d = function create4d(len1, len2, len3, len4) {
-    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(len2 == undefined ? [] : new Array(len4))));
+    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(len4 == undefined ? [] : new Array(len4))));
 }
 Array.create5d = function create4d(len1, len2, len3, len4, len5) {
-    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(new Array(len4).fillCopy(len2 == undefined ? [] : new Array(len5)))));
+    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(new Array(len4).fillCopy(len5 == undefined ? [] : new Array(len5)))));
 }
 Array.create6d = function create4d(len1, len2, len3, len4, len5, len6) {
-    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(new Array(len4).fillCopy(new Array(len5).fillCopy(len2 == undefined ? [] : new Array(len6))))));
+    return new Array(len1).fillCopy(new Array(len2).fillCopy(new Array(len3).fillCopy(new Array(len4).fillCopy(new Array(len5).fillCopy(len6 == undefined ? [] : new Array(len6))))));
 }
 
+// 獲取維度 //
+// 只針對完整的陣列
+// ### 可以的話改成變量 非函數式
+Array.prototype.getDim = function getDim() {
+    var dim = 0;
+    function count(element) {
+        if (Array.isArray(element)) {
+            dim++;
+            count(element[0]);
+        }
+    }
+    count(this);
+    return dim;
+}
+// 獲取形狀 //
+Array.prototype.getShape = function getShape() {
+    var shape = [];
+    function count(element) {
+        if (Array.isArray(element)) {
+            shape.push(element.length);
+            count(element[0]);
+        }
+    }
+    count(this);
+    return shape;
+}
+
+// 重塑 //
+Array.prototype.reshape = function reshape() {
+    // ### 未完成
+}
 
 // Four arithmetic of Array
+// 只針對類型是 number 的元素做運算
+// ### 要擴展成可以對矩陣與矩陣運算
 // 加 //
 Array.prototype.plus = function plus(value = 0) {
     if (typeof value != "number") throw notTypeofNumber;
-    return this.map(element => element + value);
+    return this.map(element => typeof element == "number" ? element + value : element);
 }
 // 減 //
 Array.prototype.minus = function minus(value = 0) {
     if (typeof value != "number") throw notTypeofNumber;
-    return this.map(element => element - value);
+    return this.map(element => typeof element == "number" ? element - value : element);
 }
 // 乘 //
 Array.prototype.multiply = function multiply(value = 1) {
     if (typeof value != "number") throw notTypeofNumber;
-    return this.map(element => element * value);
+    return this.map(element => typeof element == "number" ? element * value : element);
 }
 // 除 //
 Array.prototype.divide = function divide(value = 1) {
     if (typeof value != "number") throw notTypeofNumber;
-    return this.map(element => element / value);
+    return this.map(element => typeof element == "number" ? element / value : element);
 }
 // 模除 //
 Array.prototype.mod = function mod(value = 1) {
     if (typeof value != "number") throw notTypeofNumber;
-    return this.map(element => element % value);
+    return this.map(element => typeof element == "number" ? element % value : element);
+}
+Array.prototype.pow = function pow(value) {
+    if (typeof value != "number") throw notTypeofNumber;
+    return this.map(element => typeof element == "number" ? element ** value : element);
+}
+
+// 直和 //
+Array.prototype.directSum = function directSum(array) {
+    const thisShape = this.getShape();
+    const arrayShape = this.getShape();
+    // ### 未完成
+    
 }
 
 
@@ -230,4 +275,60 @@ class Tuple {
 }
 
 
+const _ = undefined;
 const print = console.log;
+
+// 特定範圍的連續值矩陣 //
+// 輸入1個變數 返回[0...(param = start)]
+// 輸入2個變數 返回[start...end]
+function range(start, end) {
+    if (arguments.length <= 1) {
+        return [...new Array(start).keys()];
+    } else {
+        return Array.from({length: end - start + 1}, (_, i) => i + start);
+    }
+}
+
+
+
+
+
+// ### 修改設計模式
+const extendingPrototype = {
+    typeName1: {
+        prototypeName1 : function prototypeName1() {},
+        prototypeName2 : function prototypeName2() {},
+        prototypeName3 : function prototypeName3() {},
+        // ...
+    },
+    typeName2: {
+        prototypeName1 : function prototypeName1() {},
+        prototypeName2 : function prototypeName1() {},
+        // ...
+    },
+    // ...
+}
+
+// ### 用switch
+function checkExtensionPrototypeName(extendingPrototype) {
+    for (const typeName in ExtendingPrototype) {
+        if (typeName == "Number") {
+            for (const prototypeName in typeName) {
+                if (Number.hasOwnProperty(prototypeName)) {
+                // ###
+                }
+            }
+        }
+        if (typeName == "Boolean") {
+            for (const prototypeName in typeName) {
+                if (Boolean.hasOwnProperty(prototypeName)) {
+                // ###
+                }
+            }
+        }
+        // if ...
+    }
+    
+}
+
+// Number.prototype[prototypeName] = prototypeName;
